aea684443c60407159054c01fee88d86e9bf614b
Ben <heliostatic@gmail.com>
2011-07-21 20:18:19 -0700

    Project created
    initial rails new (3.0.9)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tascit::Application.config.secret_token = '527740264121c40efb727743903307119c3fed03eb62eaa4e49ee238a0ff8bc359094fa5e5cac8aea53f3559a426b0a8a7d430309a8eae7b5bbba7da1b1a35c2'
