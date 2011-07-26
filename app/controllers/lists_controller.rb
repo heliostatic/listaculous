4293b9fe6d1a2671dce60b276fe327019877e467
Ben <heliostatic@gmail.com>
2011-07-25 19:10:22 -0700

    Shifted has_many from owner to list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   # GET /lists/1.xml
   def show
     @list = List.find(params[:id])
-
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @list }
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,2 @@
 class Owner < ActiveRecord::Base
-  has_many :tasks
 end
