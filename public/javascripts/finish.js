4ffd2c5f1aa4cf6656d289f399d72abfebd8b92a
Ian McDowell <igmcdowell@gmail.com>
2011-07-30 06:47:01 -0700

    added a comment explaing a possible use of success function. Done insomnia coding.

--- public/javascripts/finish.js
+++ public/javascripts/finish.js
@@ -5,7 +5,7 @@ $(".task").live("click", function() {
      url:'/tasks/' + $(this)[0].id,
      data:tdata,
      success: function() {
-         
+         //we may want to delay actually checking the task off until we get a response from the server. Until we get here we might display some intermediary state.
      },
      type: 'PUT', //This won't work in IE6. Sorry IE6, this is the right REST verb.   
     });
