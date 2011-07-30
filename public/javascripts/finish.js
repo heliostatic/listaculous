873bd8b0dd684cac90fcbd7fcbc0695128fca641
Ian McDowell <igmcdowell@gmail.com>
2011-07-30 05:31:57 -0700

    changed from  to [0] to operate on the interesting element (all jquery selectors return an array, even when there's only one object).

--- public/javascripts/finish.js
+++ public/javascripts/finish.js
@@ -1,8 +1,8 @@
 $(".task").live("click", function() {
-var data = [$(this).checked, $(this).id]; // the value of input that need to send to server
-alert(data);
-$.get(url, data, // make ajax request
- function(html) { // function to handle the response
-  $("#worked").html("html"); // change the inner html of update div
- });
+    var data = [$(this)[0].checked, $(this)[0].id]; // the value of input that need to send to server
+    alert(data);
+    $.get(url, data, // make ajax request
+     function(html) { // function to handle the response
+      $("#worked").html("html"); // change the inner html of update div
+     });
 });
