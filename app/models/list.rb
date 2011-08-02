cc20f23f6724655fe4e3c178801fe49417982bd0
Ben Cohen <ben@bencohen.net>
2011-08-01 19:36:28 -0700

    fixed some badness around the architecture change

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,5 @@
 class List < ActiveRecord::Base
   belongs_to :owner
-  has_many :tasks, class_name => "List",
-    :foreign_key => "parentlist_id"
-  belongs_to :parentlist, class_name => "List"
+  has_many :tasks, :class_name => 'List', :foreign_key => "parentlist_id"
+  belongs_to :parentlist, :class_name => 'List'
 end
