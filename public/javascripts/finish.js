beaba8056b7990561f64dd5c1187afcdaa88d315
Ian McDowell <igmcdowell@gmail.com>
2011-08-08 00:37:31 -0700

    added strikethrough for finished tasks and dynamic class toggling. Fixed bug with ajax submit

--- public/javascripts/finish.js
+++ public/javascripts/finish.js
@@ -1,11 +1,13 @@
 $(".task").live("click", function() {
     var status = ($(this)[0].checked)?1:0;
     var tdata = { 'id':$(this)[0].id , 'status':status }
+    var el = this;
     $.ajax({
-     url:'/lists' + $(this)[0].id,
+     url:'/lists/' + $(this)[0].id,
      data:tdata,
      success: function() {
-				
+		$(el).parent().toggleClass('finished');	
+			
          //we may want to delay actually checking the task off until we get a response from the server. Until we get here we might display some intermediary state.
      },
      type: 'PUT', //This won't work in IE6. Sorry IE6, this is the right REST verb.   
