252509b2c2e64d2669466531182071ff46532d81
Ben <heliostatic@gmail.com>
2011-08-09 19:57:15 -0700

    completed?

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,8 @@ class List < ActiveRecord::Base
   acts_as_list :scope => :parentlist
   has_many :tasks, :class_name => 'List', :foreign_key => "parentlist_id", :order => "position"
   belongs_to :parentlist, :class_name => 'List'
+  
+  def completed?
+		self.status == 0
+  end
 end
