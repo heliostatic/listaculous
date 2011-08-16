c4bb37fcfac504e2522e14a063377f03311ef6e0
Ben Cohen <ben@bencohen.net>
2011-08-16 07:36:12 -0700

    adding check, hope it fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-require "~/Dropbox/tascit/config.rb"
+require "~/Dropbox/tascit/config.rb" unless ENV['TWITTER_CONSUMER_KEY']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'] ||Config::TWITTER[:consumer_key], ENV['TWITTER_CONSUMER_SECRET'] ||Config::TWITTER[:consumer_secret]
