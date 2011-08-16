a199cb2a966c1a47b61c57917a7c7cc323a02e5f
Ben Cohen <ben@bencohen.net>
2011-08-16 07:55:50 -0700

    Cleaning up migrations

--- db/migrate/20110726025945_add_nameto_task.rb
+++
@@ -1,7 +0,0 @@
-class AddNametoTask < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20110728021033_change_task_status_to_int.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTaskStatusToInt < ActiveRecord::Migration
-  def self.up
-    change_column :tasks, :status, :integer
-  end
-
-  def self.down
-    change_column :tasks, :status, :text
-  end
-end
