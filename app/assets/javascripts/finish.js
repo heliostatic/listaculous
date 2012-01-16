e186fc3f133ac88dac1e8e6002ec61a22c85119e
Ian McDowell <igmcdowell@gmail.com>
2012-01-15 16:02:41 -0800

    Create task form now works with animations for the default case (creating a task at base list level). Still need to add callback logic to turn the placeholder task into a real task, and logic for moving the form to add tasks to child lists.

--- app/assets/javascripts/finish.js
+++ app/assets/javascripts/finish.js
@@ -23,6 +23,50 @@ function FetchChildren(list_id, el, recurse) {
 	});
 }
 
+//When enter is pressed, we should either focus to the input box, submit the input box, or blur if the input box is focused and empty.
+function focusOrSubmit() {
+    var input = $("#list_name");
+    if ( $("#list_name:focus").length ) {
+        if (input.val().length == 0){
+            input.blur();
+        }
+        else {
+            input.parent().submit();
+            parid = $('#list_parentlist_id').val();
+            //find or create the UL to hold the sublist.
+            var reful = $('#list_'+parid)[0] ? $('#list_'+parid) : $('#'+parid).append('<ul id="list_'+parid+'"></ul>');
+            var tlist = $('<li class="temporary">'+input.val()+'</li>')
+            reful.append(tlist);
+            var curx = $('#list_name').offset().left;
+            var cury = $('#list_name').offset().top;
+            var newx = $(reful).offset().left;
+            var newy = $(reful).offset().top + $(reful).outerHeight() -tlist.outerHeight();
+            tlist.css({
+                "position": "absolute",
+                "left": curx + "px",
+                "top": cury + "px"
+            }).animate({
+                "top": newy + "px",
+                "left": newx + "px"
+            }, 500, function () {
+                tlist.removeAttr('style');
+            });
+            input.val('');
+        }
+    }
+    else {
+        input.focus();
+    }
+    
+}
+
+$("body").live('keypress', function (e) {
+    if (e.keyCode == 13) {
+        focusOrSubmit();
+        e.preventDefault(); //prevents sending the carriage return to the text field.
+    }
+});
+
 $('#expandAll').live('click',function(){
   CollapseAll();
     var expanders = $('.listExpander');
--- app/assets/javascripts/inline_effects.js
+++ app/assets/javascripts/inline_effects.js
@@ -1,12 +1,4 @@
-function init() {
-    $("body").live('keypress', function (e) {
-        if (e.keyCode == 13) {
-            var parentid = $("#createPrompt").parent().attr('id');
-            MakeOrSubmitTaskForm(parentid);
-            e.preventDefault(); //prevents sending the carriage return to the text field.
-        }
-    });
-    
+function init() {    
     $('.task').live({
         mouseenter: function (e) {
             $('#adder').remove(); //beats figuring it out
@@ -38,93 +30,10 @@ function init() {
         $('#adder').remove();
     });
 
-    $("#createPrompt").live('focus', function () {
+    /*$("#createPrompt").live('focus', function () {
         MakeOrSubmitTaskForm();
-    });
+    });*/
 }
-
-function CreateTask(ref, name, parentlist_id, owner_id) {
-    var ldata = {
-        'name': name,
-        'parentlist_id': parentlist_id,
-        'owner_id': owner_id,
-        'status': 0
-    }
-    $.ajax({
-        url: '/lists',
-        data: {
-            'list': ldata
-        },
-        beforeSend: function (req) {
-            req.setRequestHeader("Accept", "text/xml");
-        },
-        success: function (data, status, responseObject) {
-            ref.removeClass('unconfirmed');
-            var newid = $(data).find('id').text();
-            ref.wrapInner(function () {
-                return '<a href="' + responseObject.getResponseHeader('Location') + '" / >';
-            });
-            // TODO make the new check boxes work (need to match the checkboxes created at load.)
-            ref.prepend('<span class="renderbug"><input class="checker" id="checkbox_' + newid + '" type="checkbox"></span>');
-            ref.attr('id', newid)
-        },
-        type: 'POST',
-        //This won't work in IE6. Sorry IE6, this is the right REST verb.   
-    });
-}
-
-function MakeOrSubmitTaskForm(parentlist_id) {
-    if ($("#newTask").length == 0) {
-        $('#createPrompt').css('display', 'none');
-        $('#createPrompt').after('<div id="newTask"><input type="text" id="taskName"/></div>');
-        $("#taskName")[0].focus();
-    } else {
-        //we have ul id and ul css id to distinguish between <li> items and their associated children uls. TODO refactor this.
-        var parentlistulid = 'list_' + parentlist_id;
-        var parentlistulcssid = '#' + parentlistulid;
-        if ($('#taskName').val().length) {
-            var task_name = $('#taskName').val();
-            var ref = $('<li class="task unconfirmed">' + task_name + '</li>');
-            if ($(parentlistulcssid).length == 0) {
-                var reful = '<ul id="' + parentlistulid + '"></ul>';
-                $('#' + parentlist_id).append(reful);
-            }
-            $(parentlistulcssid).append(ref);
-            CreateTask(ref, task_name, parentlist_id, owner_id); // these come from embedded ruby in show.html
-            var curx = $('#newTask').offset().left;
-            var cury = $('#newTask').offset().top;
-            try {
-                var newx = $(parentlistulcssid + ' li:last').offset().left;
-                var newy = $(parentlistulcssid + ' li:last').offset().top;
-            } catch (err) {
-                if (err.name == 'TypeError') {
-                    var newx = $(parentlistulcssid).offset().left;
-                    var newy = $(parentlistulcssid).offset().top;
-                } else throw (err);
-            }
-            ref.css({
-                "position": "absolute",
-                "left": curx + "px",
-                "top": cury + "px"
-            }).animate({
-                "top": newy + "px",
-                "left": newx + "px"
-            }, 500, function () {
-                ref.remove();
-                $(parentlistulcssid).append(ref);
-                ref.removeAttr('style');
-            });
-            MakeSortable($(parentlistulcssid)); //may not be necessary
-        }
-        $('#newTask').remove();
-        $('#createPrompt').css('display', 'block');
-    }
-}
-
-
-
-
-
 
 $(function () {
     MakeSortable($('.tasklist')[0]);
